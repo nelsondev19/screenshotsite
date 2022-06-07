4d858ac3c162ed2560e069eb62f57a78d56fce0c
nelsoncode019 <nelsonher019@gmail.com>
2022-06-07 10:01:07 -0600

    [FIX] configs

--- svelte.config.js
+++ svelte.config.js
@@ -8,9 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: node({ env: { port: process.env.PORT || 3000 }, out: 'build' })
+		adapter: node({ out: 'build' })
 	}
 };
 
