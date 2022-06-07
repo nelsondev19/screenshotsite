bec10ed71f51bda83e67746e59aa454ee5e0a1f2
nelsoncode019 <nelsonher019@gmail.com>
2022-06-07 09:52:07 -0600

    configs for heroku

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,16 @@
-import adapter from '@sveltejs/adapter-node';
+import node from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
+	preprocess: preprocess(),
+
 	kit: {
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		},
-		adapter: adapter({ out: 'build' })
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: node({ env: { port: process.env.PORT || 3000 }, out: 'build' })
 	}
 };
 
