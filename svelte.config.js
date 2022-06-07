31ee7348b0bbb29aaff08ab5d05bbecb402c71eb
nelsoncode019 <nelsonher019@gmail.com>
2022-06-07 08:33:38 -0600

    [FIX] enable edge functions

--- svelte.config.js
+++ svelte.config.js
@@ -11,12 +11,14 @@ const config = {
 		})
 	],
 	kit: {
-		adapter: adapter(),
-
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
-		}
+		},
+		adapter: adapter({
+			edge: true,
+			split: false
+		})
 	}
 };
 
